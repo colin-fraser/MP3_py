81e8cc4a86b50ca94ef2a3e900653a261e1dea5c
Colin Fraser <colin.fraser@chimp.net>
2018-04-08 12:44:30 -0700

    problem 4

--- TablePartD.py
+++ TablePartD.py
@@ -4,5 +4,11 @@ connection = hb.Connection()
 
 powers = connection.table('powers')
 
-row = powers.row('row1')
-print(row)
+def printrow(table, row_id, **args):
+    row = powers.row(row_id)
+    outlist = []
+    for a in args:
+        outlist.append('%s: %s' % (a, row[a]))
+    print(', '.join(outlist))
+
+printrow('row1', 'hero', 'power', 'name', 'xp', 'color')
