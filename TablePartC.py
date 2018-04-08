31ecb4a6f3b4a374a744a7262bc644fe2a176811
Colin Fraser <colin.fraser@chimp.net>
2018-04-08 12:31:16 -0700

    problem 3

--- TablePartC.py
+++ TablePartC.py
@@ -1,3 +1,13 @@
 import happybase as hb
 import csv
 
+row_names = ['personal:hero', 'personal:power', 'professional:name', 'professional:xp', 'custom:color']
+
+connection = hb.Connection()
+powers = connection.table('powers')
+
+with open('input.csv') as f:
+    reader = csv.reader(f)
+    for line in reader:
+        payload = dict(zip(row_names, line[1:]))
+        powers.put(line[0], payload)
